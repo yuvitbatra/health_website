74e2ffb981ceeb854b394bd7c586753f77c34b2c
yuvitbatra <yuvitbatra@gmail.com>
2022-08-06 14:57:26 +0530

    added footer and welcome

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import time
+
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,7 +8,11 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SECRET_KEY'] = 'data_management'
 
 @app.route('/')
-def Home():
+def loader():
+    return render_template('loader.html')
+
+@app.route('/HomePage')
+def HomePage():
     return render_template('layout.html')
 
 if __name__ == '__main__':
